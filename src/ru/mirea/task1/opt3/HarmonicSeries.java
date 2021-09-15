23e7e96ebe15991bbdcf2fe138d26cda4f589e72
Vlad <maddoge21@mail.ru>
2021-09-15 10:26:24 +0300

    Add HarmonicSeries.java in op3

---
+++ src/ru/mirea/task1/opt3/HarmonicSeries.java
@@ -0,0 +1,10 @@
+package ru.mirea.task1.opt3;
+
+public class HarmonicSeries {
+    public static void main(String[] args) {
+        System.out.println("Первые 10 чисел гармонического ряда: ");
+        for (double i = 1; i < 11; i++){
+            System.out.println(1/i + " ");
+        }
+    }
+}
