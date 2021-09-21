7785f9fdf7e57fb6a187990c4571f549c3b6c8e5
Vlad <maddoge21@mail.ru>
2021-09-22 02:08:30 +0300

    add opt1 in Task4

---
+++ src/ru/mirea/task4/opt1/AuthorTest.java
@@ -0,0 +1,8 @@
+package ru.mirea.task4.opt1;
+
+public class AuthorTest {
+    public static void main(String[] args) {
+        Author a1 = new Author("Mark Timber", "timbersaw2@gmail.com" , 'm');
+        System.out.println(a1);
+    }
+}
